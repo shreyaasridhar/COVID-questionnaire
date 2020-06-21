c1d2597652820339770541c5170c549f25cafd9b
shreyaasridhar <shreyanju@gmail.com>
2020-06-21 00:24:21 -0700

    Setup login and questionnaire flow

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, abort, jsonify, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import json
 from models import setup_db, User, Qtable, drop_create_all
+import bcrypt
 
 
 app = Flask(__name__)
@@ -13,4 +14,13 @@ CORS(app)
 
 @app.route('/')
 def index():
-    return "Hello From Server"
+    return render_template("index.html")
+
+@app.route('/login',methods = ['POST'])
+def login():
+    print(request.form)
+    return redirect(url_for('questionnaire'))
+
+@app.route('/questionnaire')
+def questionnaire():
+    return "QUESTIOINNAIR"
