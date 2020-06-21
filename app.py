6e8aa6a4153d5faf11a2fb3049add76d82410b60
shreyaasridhar <shreyanju@gmail.com>
2020-06-21 13:42:38 -0700

    Password hashes working

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, abort, jsonify, render_template, redirect, url
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import json
+from flask_migrate import Migrate
 from models import setup_db, User, Qtable, drop_create_all
 import bcrypt
 
@@ -18,9 +19,23 @@ def index():
 
 @app.route('/login',methods = ['POST'])
 def login():
-    print(request.form)
-    return redirect(url_for('questionnaire'))
+    passkey = bytes(request.form['password'],'utf-8')
+    username = request.form['username']
+    user_found = User.query.filter(User.name == username).one_or_none()
+
+    if user_found == None:
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(passkey, salt)
+        new_user = User(hashed.decode('utf-8'), username, salt)
+        new_user.insert()
+        return redirect(url_for('questionnaire'))
+    if bcrypt.checkpw(passkey, user_found.hash_key.encode('utf-8')) :
+        return redirect(url_for('questionnaire'))
+    else:
+        print("false password")
+        return "Enter correct Password"
+    
 
 @app.route('/questionnaire')
 def questionnaire():
-    return "QUESTIOINNAIR"
+    return render_template("survey.html")
--- models.py
+++ models.py
@@ -1,11 +1,13 @@
 import os
-from sqlalchemy import Column, String, Integer, create_engine, Date
+from flask import Flask
+from sqlalchemy import Column, String, Integer, create_engine, Date, Boolean
 from flask_sqlalchemy import SQLAlchemy
 import json
 
 database_name = "covid"
 database_path = "postgres://{}/{}".format('localhost:5432', database_name)
-db = SQLAlchemy()
+app = Flask(__name__)
+db = SQLAlchemy(app)
 
 
 def drop_create_all():
@@ -21,17 +23,37 @@ def setup_db(app, database_path=database_path):
     db.create_all()
 
 class User(db.Model):
-    __tablename__ = "user"
+    __tablename__ = "users"
     id = Column(Integer,primary_key = True)
-    hash = Column(String, nullable = False, unique = True)
+    hash_key = Column(String, nullable = False, unique = True)
     salt = Column(String, nullable = False)
     name = Column(String, nullable = False)
+
+    def __init__(self, hash_key, name, salt):
+        self.hash_key = hash_key
+        self.name = name
+        self.salt = salt
+    
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+
 
 class Qtable(db.Model):
     __tablename__ = "qtable"
     id = Column(Integer, primary_key = True)
     date = Column(Date, nullable = False)
-    q1 = Column(String)
-    q2 = Column(String)
-    q3 = Column(String)
+    q1 = Column(Boolean)
+    q2 = Column(Boolean)
+    q3 = Column(Boolean)
 
+    def __init__(self, date, q1, q2, q3):
+        self.date = date
+        self.q1 = q1
+        self.q2 = q2
+        self.s3 = q3
+    
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
