8ccf0165916f97d01b3d02318c77a88a977328c6
Shreyaa Sridhar <shreyanju@gmail.com>
2020-06-25 13:33:27 -0700

    Update app.py

--- app.py
+++ app.py
@@ -36,12 +36,12 @@ def create_app():
         if bcrypt.checkpw(passkey, user_found.hash_key.encode('utf-8')) :
             return redirect(url_for('questionnaire', name = username, user_id = user_found.id))
         else:
-           flash("Enter correct Password")
+           flash('Enter correct Password')
         return redirect(url_for('index'))
 
     @app.route("/forgot")
     def forgot():
-        return render_template("reset.html")
+        return render_template('reset.html')
 
     @app.route('/reset', methods = ['POST'])
     def reset_password():
@@ -49,7 +49,7 @@ def create_app():
         username = request.form['username']
         user_found = User.query.filter(User.name == username).one_or_none()
         if user_found == None:
-            flash("Invalid user")
+            flash('Invalid user')
             return redirect(url_for('index'))
         salt = bcrypt.gensalt()
         hashed = bcrypt.hashpw(passkey, salt)
@@ -64,21 +64,21 @@ def create_app():
         user_id = request.args['user_id']
         success = request.args['success'] if 'success' in request.args else None
         db_questions = Questions.query.all()
-        return render_template("survey.html", questions = [q.format()["name"] for q in db_questions], name = name, user_id = user_id, success=success)
+        return render_template('survey.html', questions = [q.format()['name'] for q in db_questions], name = name, user_id = user_id, success=success)
 
-    @app.route('/questionnaire', methods = ["POST"])
+    @app.route('/questionnaire', methods = ['POST'])
     def add_question():
         data = request.get_json()
         question = Questions(name=data['name'])
         question.insert()
-        return "Added question"
+        return 'Added question'
 
-    @app.route('/questionnaire_submit', methods = ["POST"])
+    @app.route('/questionnaire_submit', methods = ['POST'])
     def store_questionnaire():
         data = request.form
-        entry = Qtable(data['today'], data["user_id"], data['username'], [data[str(i)] == '1' for i in range(1, len(data)-2)])
+        entry = Qtable(data['today'], data['user_id'], data['username'], [data[str(i)] == '1' for i in range(1, len(data)-2)])
         entry.insert()
-        return redirect(url_for('questionnaire', name = data['username'], user_id = data["user_id"], success=True))
+        return redirect(url_for('questionnaire', name = data['username'], user_id = data['user_id'], success=True))
 
 
 
@@ -88,23 +88,23 @@ def create_app():
         print(now)
         today = Qtable.query.filter(Qtable.date == now).all()
         return jsonify({
-            "success":True,
-            "today": [t.format() for t in today],
-            "total_entries": len(today)
+            'success':True,
+            'today': [t.format() for t in today],
+            'total_entries': len(today)
         })
 
     @app.route('/date-filter')
     def filter_date():
-        return render_template("filter.html", filtered=None)
+        return render_template('filter.html', filtered=None)
 
-    @app.route('/users-by-date', methods = ["POST"])
+    @app.route('/users-by-date', methods = ['POST'])
     def user_by_date():
         date_req = request.form['date']
         dated = Qtable.query.filter(Qtable.date == date_req).all()
-        return render_template("filter.html", filtered={
-            "date": date_req,
-            "users": [t.format() for t in dated],
-            "total_entries": len(dated)
+        return render_template('filter.html', filtered={
+            'date': date_req,
+            'users': [t.format() for t in dated],
+            'total_entries': len(dated)
         })
     return app
 
